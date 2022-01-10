195c9f541b2b08e380b4d25ed47b61409b8ba42f
actuallytest <65369980+actuallytest@users.noreply.github.com>
2022-01-10 20:24:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ url = 'https://intense-beach-04157.herokuapp.com/smartLocks/'
 st.title('Smart Locks in Turkey')
 
 
-lock_models = ["Perkotek","ERD-1120","Efes Digital Panel","Mas","AC 13PX","Burg Wachter","DIP40","Lorex LR-DPH2","M100","MB05-03","MB DYF40","MLŞ 14-70","MLŞ 14-107","MRA 101","Netalsan Obsidian","ONDRIVE ED07","OP705","OP M400","OP M500","Pratik Kart","Desi Steely","Audio","Teknoline","Teknoline IMR18","Desi UTOPIC","WL02","D45","A20 Kapı Kilidi"]
+lock_models = ["Perkotek","ERD-1120","Efes Digital Panel","Mas","AC 13PX","Burg Wachter","DIP40","Lorex LR-DPH2","M100","MB05-03","MB DYF40","MLŞ 14-70","MLŞ 14-107","MRA 101","Netalsan Obsidian","ONDRIVE ED07","OP705","OP M400","OP M500","Pratik Kart","Desi Steely","Audio","Teknoline","Teknoline IMR18","Desi UTOPIC","WL02","D45","A20 Kapı Kilidi","Kwikset 275 Contemporary Deadbolt"]
 option = st.selectbox(
      'Select your Lock Model',
-     lock_models)
+     lock_models)"
 
 st.write('You selected:', option)
 
